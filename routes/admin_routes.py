4f1d9fda4bac6e32eb624d8c556bfd52be20473f
Renan Poggian <renanpoggiangomes@gmail.com>
2024-08-28 17:03:25 -0300

    add NovoProdutoDTO

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
 
+from dtos.novo_produto_dto import NovoProdutoDTO
+from repositories.produto_repo import ProdutoRepo
+from models.produto_model import Produto
 
 router = APIRouter(prefix="/manager")
 
-@router.get("/")
-async def admin_home():
-    return {"message": "Bem-vindo à área administrativa!"}
+@router.get("/obter_produtos")
+async def obter_produtos() -> list[Produto]:
+    produtos = ProdutoRepo.obter_todos()
+    return produtos
+
+@router.get("/inserir_produto")
+async def inserir_produto(produto: NovoProdutoDTO):
+    pass
